99795f9b5d356a174f41cf1c30a4274ae620a9ce
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2020-10-25 18:13:03 +0530

    ADDED:Flipping coin multiple times and storing Doublet result combinations

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -8,17 +8,18 @@ tail=0;
 read -p "Enter number of flip required " n
 for (( i=0; i<n; i++ ))
 do
-	singlet=$(( RANDOM%2 ));
-	if [ $singlet == 1 ];
-	then
-         		head=$(($head+1))
-	else
-        		tail=$(($tail+1))
-	fi
-	
-	
+doublet=$(( RANDOM%4 ));
+	case $doublet in
+		0) headhead=$(($headhead+1));;
+		1) headtail=$(($headtail+1));;
+		2) tailhead=$(($tailhead+1));;
+		3) tailtail=$(($tailtail+1));;
+		*) echo "Invalid Combination";;
+	esac
 done
 }
 flipcoincombination
-echo "Number of head win" $head
-echo "Number of tail win" $tail
+echo "Number of headhead combination " $headhead
+echo "Number of headtail combination " $headtail
+echo "Number of tailhead combination " $tailhead
+echo "Number of tailtail combination " $tailtail
