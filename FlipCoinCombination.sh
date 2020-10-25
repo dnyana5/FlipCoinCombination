c56b867a7c2607f763898a9756386139ddae38c3
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2020-10-25 17:57:41 +0530

    ADDED:Flipping a Coin to Display Head or

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 
 echo "WELCOME TO FLIP COIN COMBINATION PROGRAM ON MASTER BRANCH"
+
+
+coinToss=$((RANDOM%2))
+if [ $coinToss == 0 ]
+then
+   echo "Heads win"
+else
+   echo "Tails win"
+fi
