3a200bb93b5b4b1448bf0279a1fcd32ea6183f64
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2020-10-25 18:17:06 +0530

    ADDED:Flipping coin multiple times and storing Triplet result combinations

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,25 +1,33 @@
 #!/bin/bash -x
 
-echo "WELCOME TO FLIP COIN COMBINATION PROGRAM ON MASTER BRANCH"
-
-function flipcoincombination() {
+echo "WELCOME TO FLIP COIN COMBINATION"
+function flipcoincombination()
+ {
 head=0;
 tail=0;
 read -p "Enter number of flip required " n
 for (( i=0; i<n; i++ ))
 do
-doublet=$(( RANDOM%4 ));
-	case $doublet in
-		0) headhead=$(($headhead+1));;
-		1) headtail=$(($headtail+1));;
-		2) tailhead=$(($tailhead+1));;
-		3) tailtail=$(($tailtail+1));;
-		*) echo "Invalid Combination";;
+triplet=$(( RANDOM%7 ));
+        case $triplet in
+                0) headheadhead=$(($headheadhead+1));;
+                1) headheadtail=$(($headheadtail+1));;
+                2) headtailhead=$(($headtailhead+1));;
+                3) headtailtail=$(($headtailtail+1));;
+					 4) tailheadhead=$(($headtailhead+1));;
+                5) tailheadtail=$(($tailheadtail+1));;
+                6) tailtailhead=$(($tailtailhead+1));;
+                7) tailtailtail=$(($tailtailtail+1));;
+	*) echo "Invalid combination";;
 	esac
 done
 }
 flipcoincombination
-echo "Number of headhead combination " $headhead
-echo "Number of headtail combination " $headtail
-echo "Number of tailhead combination " $tailhead
-echo "Number of tailtail combination " $tailtail
+echo "Number of headheadhead combination " $headheadhead
+echo "Number of headheadtail combination " $headheadtail
+echo "Number of headtailhead combination " $headtailhead
+echo "Number of headtailtail combination " $headtailtail
+echo "Number of tailheadhead combination " $tailheadhead
+echo "Number of tailheadtail combination " $tailheadtail
+echo "Number of tailtailhead combination " $tailtailhead
+echo "Number of tailtailtail combination " $tailtailtail
