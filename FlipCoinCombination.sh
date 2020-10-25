b443fe7c1b36db58c9f98f24a26c50f9c31dd9f6
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2020-10-25 18:08:32 +0530

    ADDED:Flipping coin multiple times and storing singlet result combinations in dictionary

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -2,10 +2,23 @@
 
 echo "WELCOME TO FLIP COIN COMBINATION PROGRAM ON MASTER BRANCH"
 
-coinToss=$((RANDOM%2))
-if [ $coinToss == 0 ]
-then
-   echo "Heads win"
-else
-   echo "Tails win"
-fi
+function flipcoincombination() {
+head=0;
+tail=0;
+read -p "Enter number of flip required " n
+for (( i=0; i<n; i++ ))
+do
+	singlet=$(( RANDOM%2 ));
+	if [ $singlet == 1 ];
+	then
+         		head=$(($head+1))
+	else
+        		tail=$(($tail+1))
+	fi
+	
+	
+done
+}
+flipcoincombination
+echo "Number of head win" $head
+echo "Number of tail win" $tail
